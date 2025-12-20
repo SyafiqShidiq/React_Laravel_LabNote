aed7f0122ff8b784e09695cc596e00c98b3c3f69
SyafiqShidiq <isseiarif@gmail.com>
2025-12-20 20:51:42 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>React + PW2</h1>
+      <p>Ini adalah halaman React pertama kalian.</p>
+    </main>
+  );
+}
+
+export default App;
